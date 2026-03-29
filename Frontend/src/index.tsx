515daf64e964fdf2093d3e46c67c3ca7fb15e408
Valerie Nielson <valerie.nielson@atomicobject.com>
2026-03-29 00:20:04 -0400

    bind to 0.0.0.0 instead of 127.0.0.1

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -2,6 +2,7 @@ import { serve } from "bun";
 import index from "./index.html";
 
 const server = serve({
+  hostname: "0.0.0.0",
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
